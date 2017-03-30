20880d7406ed7e1a9549dca4643334428dc854bd
Martin Klose <martin.klose@klosebrothers.de>
2017-03-30 18:09:53 +0200

    make Approval work

---
+++ Java/src/test/java/net/davidtanzer/babysteps/BabystepsTimerTest.java
@@ -0,0 +1,22 @@
+package net.davidtanzer.babysteps;
+
+import org.approvaltests.Approvals;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class BabystepsTimerTest {
+
+    /*
+     - new
+     - start
+        - stop
+        - reset
+     - quite
+     - time passed
+
+     */
+    @Test @Ignore
+    public void testName() throws Exception {
+        Approvals.verify("");
+    }
+}
