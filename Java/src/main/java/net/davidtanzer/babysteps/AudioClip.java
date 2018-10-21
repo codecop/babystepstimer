3c7058a9bec44810bb5150a48ca24f877c88b77e
Peter Kofler <peter.kofler@code-cop.org>
2018-10-21 12:26:43 +0200

    Add extra lines.

--- Java/src/main/java/net/davidtanzer/babysteps/AudioClip.java
+++ Java/src/main/java/net/davidtanzer/babysteps/AudioClip.java
@@ -4,5 +4,7 @@ package net.davidtanzer.babysteps;
  * Abstraction of (sub domain) audio technology.
  */
 public interface AudioClip {
+
     void play(String url);
+
 }
--- Java/src/main/java/net/davidtanzer/babysteps/SystemTimer.java
+++ Java/src/main/java/net/davidtanzer/babysteps/SystemTimer.java
@@ -4,8 +4,10 @@ package net.davidtanzer.babysteps;
  * Implementation of getting the current time.
  */
 public class SystemTimer implements Timer {
+
     @Override
     public long getTime() {
         return System.currentTimeMillis();
     }
+
 }
